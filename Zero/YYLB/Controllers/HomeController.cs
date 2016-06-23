43ba73c339efaae20e83b0919adde2e675e233e3
swfightforyl <swfightforyl@163.com>
2016-06-23 09:31:28 +0800

    基础知识

--- Zero/YYLB/Controllers/HomeController.cs
+++ Zero/YYLB/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using YYLB.Models;
 namespace YYLB.Controllers
 {
     public class HomeController : Controller
@@ -13,5 +13,24 @@ namespace YYLB.Controllers
         {
             return View();
         }
+
+        //直接在对应的视图页面写死的html
+        public ActionResult ListAllTheProducts()
+        {
+            return View();
+        }
+
+        public ActionResult ShowAllTheProducts()
+        {
+            //手动提供一些集合数据
+            List<Product> products = new List<Product> {
+                new Product { ProductName="Apple",Price="10"},
+                new Product { ProductName="IPhone",Price="4000"},
+                new Product { ProductName="Dell",Price="500"}
+            };
+            return View(products);  //将products集合传递给视图，让视图去展现
+
+        }
+
     }
 }
---
+++ Zero/YYLB/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YYLB.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Price { get; set; }
+    }
+}
